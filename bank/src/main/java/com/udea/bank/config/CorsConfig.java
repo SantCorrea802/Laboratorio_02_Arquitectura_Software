088da9d9db5777c5d3fb35361b8350fb5e237c57
SantCorrea802 <santiago.correa7@udea.edu.co>
2026-04-14 00:12:47 -0500

    configure cors

--- bank/src/main/java/com/udea/bank/config/CorsConfig.java
+++ bank/src/main/java/com/udea/bank/config/CorsConfig.java
@@ -6,12 +6,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://192.168.0.21:3000" )
+    public void addCorsMappings(CorsRegistry registro) {
+        registro.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://localhost:3000",
+                        "https://bank-front-juv5.onrender.com"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }
